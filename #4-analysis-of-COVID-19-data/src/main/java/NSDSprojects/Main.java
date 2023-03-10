5f2fdfe1aaa32a8223b7348a2a8d91ee639ac30d
Andrea Restelli <andrearestelli28@gmail.com>
2023-03-10 12:22:52 +0100

    added dataset and initialized project

--- #4-analysis-of-COVID-19-data/src/main/java/NSDSprojects/Main.java
+++ #4-analysis-of-COVID-19-data/src/main/java/NSDSprojects/Main.java
@@ -1,7 +1,18 @@
 package NSDSprojects;
 
+import org.apache.spark.sql.SparkSession;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String master = args.length > 0 ? args[0] : "local[4]";
+        final String inputDatasetPath = args.length > 1 ? args[1] : "./files/ecdc_data.csv";
+        final boolean logOnConsole = args.length > 2 ? args[2].equals("true") : false;
+
+        final SparkSession spark = SparkSession
+                .builder()
+                .master(master)
+                .appName("Covid19DataAnalysis")
+                .getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
     }
 }
